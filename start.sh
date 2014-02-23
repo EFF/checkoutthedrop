74c13cff390e2d293404441088a9088b046da1a8
francois <frxpoulin@gmail.com>
2014-02-23 13:20:58 -0500

    add the bower install instruction

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./node_modules/.bin/grunt build && ./node_modules/.bin/coffee ./server/app.coffee
+./node_modules/.bin/bower install && ./node_modules/.bin/grunt build && ./node_modules/.bin/coffee ./server/app.coffee
